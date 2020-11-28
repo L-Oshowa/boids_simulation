ee473d550c525d4dfb8656501ce255ada0bc0689
j.Charriere <jonathan.charriere@hotmail.com>
2020-11-28 01:04:33 +0100

    bliblablo

--- python_test/HW.py
+++ python_test/HW.py
@@ -2,3 +2,5 @@ print('hello world')
 print('hey hello you !')
 print('message ecrit sur le master en parallel de la premiere branche')
 print('ce message est ecrit du haut de ma premiere branche')
+
+print('testest')
