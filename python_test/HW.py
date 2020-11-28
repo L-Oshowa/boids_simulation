0abd9ed8a4bdbe4b2218b0694f27d7dc5031d184
j.Charriere <jonathan.charriere@hotmail.com>
2020-11-28 17:45:03 +0100

    supression des inutiles

--- python_test/HW.py
+++
@@ -1,6 +0,0 @@
-print('hello world')
-print('hey hello you !')
-print('message ecrit sur le master en parallel de la premiere branche')
-print('ce message est ecrit du haut de ma premiere branche')
-print('test de changement de branche transfer github')
-print('testest')
