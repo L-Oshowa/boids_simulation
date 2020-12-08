fe2bb149250aeef2350e370ef1ee16dcea1d3685
jCharriere <jonathan.charriere@hotmail.come>
2020-12-08 23:57:52 +0100

    fonction finding neighbour fini

--- world.py
+++ world.py
@@ -25,6 +25,9 @@ class world:
         self.b_cond = b_cond
         if self.b_cond == 2: # if the border_cond is black hole, make an "real windows" with a size = to normal size + twice the biggest boids range.
             self.real_w = w+2*max(l_view_range)
+        elif self.b_cond == 1:
+            if max(l_view_range) >= min(self.w) :
+                print('view range to big for the simulation with b_cond = 1')
         self.l_boids = []
         for ii in range(n_boids):
             self.l_boids.append(Boids(l_pos[det_in(l_pos,ii)]+max(l_view_range)*(self.b_cond==2), \
@@ -34,7 +37,7 @@ class world:
                 l_view_range[det_in(l_view_range,ii)], \
                 l_view_angle[det_in(l_view_angle,ii)]))
         self.l_obstacle = [] #WIP     
-    def position(self,pos) : #change pos according to border condition
+    def position(self,pos) : #change pos according to border condition NEED ALSO TO CHANGE SPEED
         if self.b_cond == 0 : #hard wall
             for ii in range(len(pos)) :
                 if pos[ii] < 0 :
@@ -48,13 +51,37 @@ class world:
                 elif pos[ii] > self.w[ii] :
                     pos[ii] = pos[ii]-self.w[ii]
         return
-    def finding_neighbour(self,boids) : #determine the neighbour of a given boids, depending his view range and angle and on the border cond. 
+    def finding_neighbour(self,ii_boids) : #determine the neighbours of the l_boids[ii], depending on his view range and angle and on the border cond. In case of b_cond == 1, also return a list of array for the desired boids position
         # optimisation possibility : slice space in cube or, if avoid the calcule of distance between boids doubled
         l_neighbour = []
-        if b_cond == 0 : # hard wall case : the line of sight is stoped by the border
+        if self.b_cond == 0 : # hard wall case : the line of sight is stoped by the border
             for ii in range(len(self.l_boids)) :
-                vec = sel-l_boids[ii].pos-boids.pos
-                if np.linalg.norm(vec,2) <= boids.view_range :
-                    if np.arccos(np.dot(boids.v,vec)/np.linalg.norm(boids.v,2)/np.linalg.norm(vec,2)) <= boids.view_angle :
-                        l_neighbour.append(ii)
-        elif b_cond == 1 : # end=start case : to have a perfect simu, we should solve the torus geodesic. to complicate -> 
+                if ii != ii_boids :
+                    vec = self.l_boids[ii].pos-self.l_boids[ii_boids].pos
+                    if np.linalg.norm(vec,2) <= boids.view_range :
+                        if np.arccos(np.dot(self.l_boids[ii_boids].v,vec)/np.linalg.norm(self.l_boids[ii_boids].v,2)/np.linalg.norm(vec,2)) <= self.l_boids[ii_boids].view_angle :
+                            l_neighbour.append(ii)
+            return l_neighbour
+
+        elif self.b_cond == 1 : # end=start case : to have a perfect simu, we should solve the torus geodesic. to complicate -> we deliminate the max view range < min w
+            l_calc_pos = []
+            for ii in range(len(self.l_boids)) :
+                if ii != ii_boids :
+                    vec = self.l_boids[ii].pos-self.l_boids[ii_boids].pos
+                    calc_pos = 1*(vec > self.w)-1*(vec < -self.w)
+                    vec += case_shift*self.w
+                    if np.linalg.norm(vec,2) <= boids.view_range :
+                        if np.arccos(np.dot(self.l_boids[ii_boids].v,vec)/np.linalg.norm(self.l_boids[ii_boids].v,2)/np.linalg.norm(vec,2)) <= self.l_boids[ii_boids].view_angle :
+                            l_neighbour.append(ii)
+                            l_calc_pos.append(calc_pos)
+            return l_neighbour, l_calc_pos
+
+        elif self.b_cond == 2 : # black-hole case : the line of sight is stoped by the real border
+            for ii in range(len(self.l_boids)) :
+                if ii != ii_boids :
+                    vec = self.l_boids[ii].pos-self.l_boids[ii_boids].pos
+                    if np.linalg.norm(vec,2) <= boids.view_range :
+                        if np.arccos(np.dot(self.l_boids[ii_boids].v,vec)/np.linalg.norm(self.l_boids[ii_boids].v,2)/np.linalg.norm(vec,2)) <= self.l_boids[ii_boids].view_angle :
+                            l_neighbour.append(ii)
+            return l_neighbour
+                        
