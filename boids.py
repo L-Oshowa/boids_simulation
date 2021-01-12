9485f33f49822340b32d457301067d5bd3cd9678
fbierlai <francoisbierlaire@gmail.com>
2021-01-12 14:29:56 +0100

    Rework class boids

--- boids.py
+++ boids.py
@@ -1,22 +1,60 @@
-'''The Boids class has for purpose to embody the boids with the argument and fonction linked to it
-argument :
-    x : x position of the boids
-    y : y position of the boids
-    vx : x speed of the boids (position/cicle)
-    vy : y speed of the boids (position/cicle)
-    maxv : max speed
-    maxa : max acceleration
-    view_range : range in which the boids take the other boids in account
-    view_angle : angle of view in which the boids take the other boids in account (determined from the speed direction, on the both side)
-function : 
-    __init__ : init function
-    '''
+import numpy as np
 
 class Boids :
+    '''The Boids class has for purpose to embody the boids with the argument and fonction linked to it
+    argument :
+        x : x position of the boids
+        y : y position of the boids
+        vx : x speed of the boids (position/cicle)
+        vy : y speed of the boids (position/cicle)
+        maxv : max speed
+        maxa : max acceleration
+        view_range : range in which the boids take the other boids in account
+        view_angle : angle of view in which the boids take the other boids in account (determined from the speed direction, on the both side)
+    function :
+        __init__ : init function
+        '''
+
     def __init__(self,pos,v,maxv,maxa,view_range,view_angle) :
         self.pos = pos
         self.v = v
+        self.mass = 1
+        self.attraction = 1
         self.maxv = maxv
         self.maxa = maxa
         self.view_range = view_range
         self.view_angle = view_angle
+        self.steering_coeff = [1, 1, 1]
+
+    def steering(self, list_neighbour):
+        nbr_neighbour = len(list_neighbour)
+        force_separation = np.array([0, 0])
+        center_gravity = np.array([0, 0])
+        alignment = np.array([0, 0])
+        total_attraction = 0
+
+        for i in range(nbr_neighbour):
+
+            # separation computation
+            separation = list_neighbour[i].pos - self.pos
+            force_separation = force_separation -separation / np.square(np.linalg.norm(separation))
+
+            # cohesion computation
+            neighbour_attraction = list_neighbour[i].attraction
+            total_attraction += neighbour_attraction
+            center_gravity += list_neighbour[i].pos * neighbour_attraction
+
+            # alignment computation
+            alignment = alignment + list_neighbour[i].v/nbr_neighbour
+
+        center_gravity = center_gravity / total_attraction
+        alignment -= self.v
+
+        force = self.steering_coeff[0] * force_separation\
+              + self.steering_coeff[1] * (center_gravity-self.pos)\
+              + self.steering_coeff[2] * alignment
+
+        acceleration = force / self.mass
+        new_v = acceleration + self.v
+        new_pos = self.pos + self.v
+        return new_pos, new_v
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import Interface_Graphique as UI
 import time
 import boids
+import numpy as np
 
 # Temporary: initialise boids
 posX = [110, 200, 400]
@@ -8,14 +9,15 @@ posY = [120, 150, 250]
 speedX = [10, 10, -20]
 speedY = [10, 0, -30]
 
-b1 = boids.Boids([110, 120], [10, 10], 0, 0, 0, 0)
-b2 = boids.Boids([200, 150], [10, 1], 0, 0, 0, 0)
-b3 = boids.Boids([400, 250], [-20, -30], 0, 0, 0, 0)
+b1 = boids.Boids(np.array([110, 120]), np.array([10, 10]), 0, 0, 0, 0)
+b2 = boids.Boids(np.array([200, 150]), np.array([10, 0]), 0, 0, 0, 0)
+b3 = boids.Boids(np.array([400, 250]), np.array([-30, -20]), 0, 0, 0, 0)
 
 list_boids = [b1, b2, b3]
 
 # Create the window
 window = UI.MainFrame(list_boids, 500, 500)
+
 
 for y in range(20):
     time.sleep(0.1)
@@ -32,3 +34,4 @@ for y in range(20):
 
     window.boid_window.update(pos1, pos2, s1, s2)  # Update the position of the boids
 
+b1.steering([b2, b3])
