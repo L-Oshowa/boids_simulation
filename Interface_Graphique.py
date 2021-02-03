67d4c6d46b300a9f1f741962019a7f833706cc51
fbierlai <francoisbierlaire@gmail.com>
2021-02-03 12:04:37 +0100

    add-remove button

--- Interface_Graphique.py
+++ Interface_Graphique.py
@@ -3,6 +3,9 @@ import math
 import asyncio
 import simulation
 import boids
+import random
+import numpy as np
+import time
 
 
 async def start_simulation(simu, l_boids, frame, frame_rate=1/20):
@@ -19,11 +22,14 @@ async def start_simulation(simu, l_boids, frame, frame_rate=1/20):
     """
     try:
         while True:
+            t0 = time.perf_counter()
             simu.time_step(l_boids)
             frame.update([x.pos[0] for x in l_boids], [x.pos[1] for x in l_boids],
                          [x.v[0] for x in l_boids],
                          [x.v[1] for x in l_boids])
-            await asyncio.sleep(frame_rate)
+            t1 = time.perf_counter() - t0
+            if t1 < frame_rate:
+                await asyncio.sleep(frame_rate-t1)
     finally:
         pass
 
@@ -76,13 +82,13 @@ class BoidFrame:
 
     def __init__(self, master, canvas, l_boids):
 
-        self.nbr_boids = len(l_boids)
+        nbr_boids = len(l_boids)
         self.master = master
         self.canvas = canvas
 
         self.drawing = [self.canvas.create_polygon(
                                 self.polygon(l_boids[x].pos[0], l_boids[x].pos[1], l_boids[x].v[0], l_boids[x].v[1])
-                        ) for x in range(self.nbr_boids)]
+                        ) for x in range(nbr_boids)]
 
     @staticmethod
     def polygon(x, y, sx, sy):
@@ -96,10 +102,25 @@ class BoidFrame:
         return [x+(sy*l3-sx*l2)/s, y-(sx*l3+sy*l2)/s, x+(sx*l1)/s, y+(sy*l1)/s, x-(sy*l3+sx*l2)/s, y+(sx*l3-sy*l2)/s]
 
     def update(self, x_position, y_position, x_speed, y_speed):
+        n_draw_boids = len(x_position)
+        n_boids = len(self.drawing)
+        if n_draw_boids <= n_boids:
+            for x in range(n_boids):
+                if x <= n_draw_boids:
+                    self.canvas.coords(self.drawing[x], self.polygon(x_position[x], y_position[x], x_speed[x], y_speed[x]))
+                else:
+                    self.drawing.pop(x)
+        else:
+            for x in range(n_draw_boids):
+                if x <= n_boids:
+                    self.canvas.coords(self.drawing[x], self.polygon(x_position[x], y_position[x], x_speed[x], y_speed[x]))
+                else:
+                    self.add_draw_boid([x_position[x], y_position[x]], [x_speed[x], y_speed[x]])
 
-        for x in range(self.nbr_boids):
-            self.canvas.coords(self.drawing[x], self.polygon(x_position[x], y_position[x], x_speed[x], y_speed[x]))
         self.master.update()
+
+    def add_draw_boid(self, pos, v):
+        self.drawing.append(self.canvas.create_polygon(self.polygon(pos[0], pos[1], v[0], v[1])))
 
 
 class MainFrame:
@@ -133,7 +154,8 @@ class MainFrame:
     def __init__(self, n_boids, l_pos, l_v, l_maxv, l_maxa, l_view_range, l_view_angle, w, b_cond):
 
         self.simu = simulation.Simulation(w, b_cond)
-        self.n_boids=n_boids
+        self.n_boids = n_boids
+        self.w = w
 
         self.l_boids = [boids.Boids(l_pos[self.simu.det_in(l_pos, ii)],
                                     l_v[self.simu.det_in(l_v, ii)],
@@ -168,6 +190,12 @@ class MainFrame:
         self.slider_alignment.set(100)
         self.slider_alignment.grid(row=2, column=4)
 
+        self.b_add = tk.Button(self.root, text='Add', command=self.add_boids)
+        self.b_add.grid(row=3, column=1)
+
+        self.b_remove = tk.Button(self.root, text='Remove', command=self.remove_boids)
+        self.b_remove.grid(row=3, column=3)
+
         self.root.update()
 
         self.f = asyncio.ensure_future(start_simulation(self.simu, self.l_boids, self.boid_window))
@@ -175,11 +203,14 @@ class MainFrame:
 
     def start_button(self):
         if self.b_start['text'] == 'Start':
+            self.n_boids = len(self.l_boids)
             self.b_start['text'] = 'Stop'
             self.f = asyncio.ensure_future(start_simulation(self.simu, self.l_boids, self.boid_window))
             self.slider_separation.configure(state='disabled')
             self.slider_cohesion.configure(state='disabled')
             self.slider_alignment.configure(state='disabled')
+            self.b_add.configure(state='disabled')
+            self.b_remove.configure(state='disabled')
 
             percent = [self.slider_separation.get()/100, self.slider_cohesion.get()/100, self.slider_alignment.get()/100]
 
@@ -192,3 +223,24 @@ class MainFrame:
             self.slider_separation.configure(state='active')
             self.slider_cohesion.configure(state='active')
             self.slider_alignment.configure(state='active')
+            self.b_add.configure(state='active')
+            self.b_remove.configure(state='active')
+
+    def add_boids(self):
+        margin = 12
+        pos = np.array([random.randint(margin, self.w[0]-margin), random.randint(margin, self.w[1]-margin)])
+        maxv = 10
+        v = np.array([random.uniform(-maxv, maxv), random.uniform(-maxv, maxv)])
+        if v[0] == 0 and v[1] == 0:
+            v = [0.01, 0.01]
+        maxa = 1
+        view_range = 100
+        view_angle = 80
+        self.l_boids.append(boids.Boids(pos, v, maxv, maxa, view_range, view_angle))
+        self.boid_window.add_draw_boid(pos, v)
+
+    def remove_boids(self):
+        self.l_boids.pop()
+        l = len(self.boid_window.drawing)
+        self.boid_window.canvas.delete(self.boid_window.drawing[l-1])
+        self.boid_window.drawing.pop()
